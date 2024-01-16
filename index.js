b9b34b7b297799057333c7e96645aefcd83b5af1
admin <admin@adminui-MacBookPro.local>
2024-01-16 10:55:10 +0900

    style: Card 컴포넌트 스타일 지정

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-import Card from "./src/components/cards";
+import Card from "./src/components/cards.js";
 
+function createCard() {
+    const test2 = document.getElementById('todo');
+
+    const test = Card();
+    test2.appendChild(test);
+}
+createCard();
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,11 +1,15 @@
 export default function Card() {
     // 300 120
     const card = document.createElement('div');
+    card.className = 'card';
+
     card.innerHTML = `
-    <input type="text" placeholder="제목을 입력하세요."></input>
-    <input type="text" placeholder="내용을 입력하세요."></input>
-    <button>취소</button>
-    <button>등록</button>
+    <input class="title" type="text" placeholder="제목을 입력하세요."></input>
+    <input class="content" type="text" placeholder="내용을 입력하세요."></input>
+    <div class="btnbox">
+        <button class="cancel">취소</button>
+        <button class="register">등록</button>
+    </div>
     `;
     return card;
 }
