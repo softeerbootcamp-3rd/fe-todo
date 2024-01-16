9d7a54a637311babe982ebe32701258f69bb5764
admin <admin@adminui-MacBookPro.local>
2024-01-16 11:12:32 +0900

    feat: Column 기능 구현

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import Card from "./src/components/cards.js";
+import Column from "./src/components/columns.js";
 
-function createCard() {
-    const test2 = document.getElementById('todo');
+function createCard(id) {
+    const test2 = document.getElementById(id);
 
     const test = Card();
     test2.appendChild(test);
 }
-createCard();
+
+function createColumn(title, id) {
+    const test = document.getElementById('app');
+    const test2 = Column(title, id);
+
+    test.appendChild(test2);
+}
+createColumn('해야할 일', 'todo');
+createCard('todo');
---
+++ src/components/columns.js
@@ -0,0 +1,14 @@
+// import Card from "./cards";
+
+export default function Column(title, id) {
+    const column = document.createElement('div');
+    column.className = 'column';
+    column.id = id;
+
+    column.innerHTML = `
+    <span>${title}</span>
+    <span>count</span>
+    <span>+</span>
+    <span>X</span>`;
+    return column;
+}
