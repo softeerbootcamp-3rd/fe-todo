1531fd69e3e8bdde12bb339855cfd9b453030f6a
sean2337 <sean2337@naver.com>
2024-01-17 11:21:02 +0900

    feat: todoList 컴포넌트 작성
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

---
+++ src/components/todoList/index.js
@@ -0,0 +1,36 @@
+import styles from "./todoList.module.scss";
+import plusIcon from "../../asset/img/plus.svg";
+import closedIcon from "../../asset/img/closed.svg";
+import todoItem from "../todoItem";
+
+export default function todoList(parent, props) {
+  parent.innerHTML = `
+        <div class="${styles.todoList}">
+            <div class="${styles.todoList__header}">
+                <div class="${styles.todoList__countWrapper}">
+                    <h2 class="${styles.todoList__headerTitle}">${props.title}</h2>
+                    <p class="${styles.todoList__count}">${props.count}</p>
+                </div>
+                
+                <div class="${styles.todoList__btnContainer}">
+                    <button class="${styles.todoList__actionBtn}">
+                        <img src="${plusIcon}">
+                    </button>
+                    <button class="${styles.todoList__actionBtn}">
+                        <img src="${closedIcon}">
+                    </button>
+                </div>
+            </div>
+
+            <div todo-data="items" class="${styles.todoList__content}">
+            </div>
+        </div>
+    `;
+
+  const itemsContainer = parent.querySelector('[todo-data="items"]');
+  for (const item of props.items) {
+    const todoItemWrapper = document.createElement("div");
+    todoItem(todoItemWrapper, item);
+    itemsContainer.appendChild(todoItemWrapper);
+  }
+}
