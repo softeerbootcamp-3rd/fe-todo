cbbe42f214908c76b68cb7552f1267e2f0c1894c
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-18 11:14:26 +0900

    feat: mock데이터 세팅 및 기본 column 표시

--- src/components/column-container/index.js
+++ src/components/column-container/index.js
@@ -1,13 +1,21 @@
-import * as Column from "../column/index.js";
+import * as Column from '../column/index.js'
 
-export function template(data) {
+export function template({ columns }) {
   return `
     <main class="columns surface-alt">
-        ${data.map(() => Column.template()).join("")}
+        ${columns.map((column) => Column.template({ column })).join('')}
     </main>
-    `;
+    `
 }
 export function render(parent) {
-  // FIXME 초기 데이터 넣어주기
-  parent.insertAdjacentHTML("beforeend", template([1, 2, 3]));
+  // FIXME
+  // 초기 데이터 넣어주기
+  // localStorage에서 불러오기
+  parent.insertAdjacentHTML('beforeend', template({ columns: mockData }))
 }
+
+const mockData = [
+  { id: 1, columnName: '해야할 일', cards: [] },
+  { id: 2, columnName: '하고 있는 일', cards: [] },
+  { id: 3, columnName: '완료한 일', cards: [] },
+]
