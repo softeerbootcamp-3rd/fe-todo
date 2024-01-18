8936dd5d278cf765ce1f231241595ee27ae7a09d
sean2337 <sean2337@naver.com>
2024-01-18 10:50:20 +0900

    chore: 커멘트 추가

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -15,12 +15,13 @@ export default function modal(parent, props) {
 
   const modalSection = parent.querySelector('[todo-data="modalSection"]');
   modalSection.addEventListener("click", (e) => {
+    //app/index.js에서 선언해준 modal전체 영역을 눌렀을 때, 모달이 꺼지는 함수를 방지
     e.stopPropagation();
-    //FIXME..
   });
 
   const cancelBtn = parent.querySelector('[todo-data="cancelBtn"]');
   cancelBtn.addEventListener("click", () => {
+    //밖에 클릭하면 나가는 함수를 취소 버튼에도 적용
     parent.click();
   });
 
