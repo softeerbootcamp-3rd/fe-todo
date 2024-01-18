2ddfcfcc96c2f70bc17b9100ef1d537c95a0ca36
sean2337 <sean2337@naver.com>
2024-01-18 16:27:47 +0900

    refactor: 재사용되는 css 스타일 global로 옮기기 + 너무 긴 template string  함수로 분리
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/components/todoItem/index.js
+++ src/components/todoItem/index.js
@@ -3,29 +3,7 @@ import closedIcon from "../../asset/img/closed.svg";
 import editIcon from "../../asset/img/edit.svg";
 
 export default function todoItem(parent, props) {
-  parent.innerHTML = `
-        <div class="${styles.todoItem}">
-            <div>
-                <textarea type="text" rows="1" todo-data="title" class="${styles.todoItem__itemTitle}" placeholder="제목을 입력하세요">${props.title}</textarea>
-                <textarea type="text" rows="1" todo-data="content" class="${styles.todoItem__itemContent}" placeholder="내용을 입력하세요">${props.content}</textarea>
-                <div class="${styles.todoItem__bottomContainer}">
-                    <p todo-data="author" class="${styles.todoItem__itemAuthor}">author by ${props.authorName}</p>
-                    <button todo-data="cancelBtn" class="${styles["todoItem__btn--inactive"]}">취소</button>
-                    <button todo-data="submitBtn" class="${styles["todoItem__btn--active"]}">등록</button>
-                </div>
-            </div>
-
-
-            <div todo-data="actionBtnContainer" class="${styles.todoItem__actionBtnContainer}">
-                <button todo-data="eraseBtn" class="actionBtn">
-                    <img class="actionBtn__closed" src="${closedIcon}">
-                </button>
-                <button todo-data="editBtn" class="actionBtn">
-                    <img src="${editIcon}">
-                </button>
-            </div>
-        </div>
-    `;
+  parent.innerHTML = template(props);
 
   const titleNode = parent.querySelector('[todo-data="title"]');
   const contentNode = parent.querySelector('[todo-data="content"]');
@@ -127,4 +105,57 @@ export default function todoItem(parent, props) {
   dynamicTextAreaHeight(titleNode);
   dynamicTextAreaHeight(contentNode);
   setViewMode();
+}
+
+// todoItem 컴포넌트 템플릿
+function template(props) {
+  return `
+  <div class="${styles.todoItem}">
+    <div>
+      <textarea
+        type="text"
+        rows="1"
+        todo-data="title"
+        class="${styles.todoItem__itemTitle}"
+        placeholder="제목을 입력하세요"
+      >${props.title}</textarea>
+      <textarea
+        type="text"
+        rows="1"
+        todo-data="content"
+        class="${styles.todoItem__itemContent}"
+        placeholder="내용을 입력하세요"
+      >${props.content}</textarea>
+      <div class="${styles.todoItem__bottomContainer}">
+        <p todo-data="author" class="${styles.todoItem__itemAuthor}">
+          author by ${props.authorName}
+        </p>
+        <button
+          todo-data="cancelBtn"
+          class="${styles["todoItem__btn--inactive"]}"
+        >
+          취소
+        </button>
+        <button
+          todo-data="submitBtn"
+          class="${styles["todoItem__btn--active"]}"
+        >
+          등록
+        </button>
+      </div>
+    </div>
+
+    <div
+      todo-data="actionBtnContainer"
+      class="${styles.todoItem__actionBtnContainer}"
+    >
+      <button todo-data="eraseBtn" class="actionBtn">
+        <img class="actionBtn__closed" src="${closedIcon}" />
+      </button>
+      <button todo-data="editBtn" class="actionBtn">
+        <img src="${editIcon}" />
+      </button>
+    </div>
+  </div>
+`;
 }
