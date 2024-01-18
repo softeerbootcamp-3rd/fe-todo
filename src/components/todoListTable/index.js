22586908ad704f8308f29eb93d8a98beea79e9ba
sean2337 <sean2337@naver.com>
2024-01-18 16:29:02 +0900

    refactor: mock api 함수 사용하도록 수정

--- src/components/todoListTable/index.js
+++ src/components/todoListTable/index.js
@@ -1,21 +1,22 @@
 import todoList from "../todoList";
 import styles from "./todoListTable.module.scss";
+import { getTodoList } from "../../utils/API/todoList";
 
 export default function todoListTable(parent, props) {
+  //행 + 데이터를 모두 감싸고 있는 컨테이너 (테이블)
   parent.innerHTML = `
     <div todo-data="todoListTable" class="${styles.todoListTable}">
-        
     </div>
     `;
 
+  //데이터 API호출 후, 각 todoList에 넣어서 각각 만들어주고 그것을 마운트해준다.
   const todoListTable = parent.querySelector('[todo-data="todoListTable"]');
-
-  // 컬럼 생성
-  const todoDataEntries = Object.entries(props.todoData);
-
-  for (const [todoColName, todoColData] of todoDataEntries) {
-    const container = document.createElement("div");
-    todoList(container, { title: todoColName, items: todoColData });
-    todoListTable.appendChild(container);
-  }
+  getTodoList().then((todoData) => {
+    const todoDataEntries = Object.entries(todoData);
+    for (const [todoColName, todoColData] of todoDataEntries) {
+      const container = document.createElement("div");
+      todoList(container, { title: todoColName, items: todoColData });
+      todoListTable.appendChild(container);
+    }
+  });
 }
