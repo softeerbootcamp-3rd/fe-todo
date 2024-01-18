bc7d9d92fbbb02ffb1b471f4b256362ed4072f23
sean2337 <sean2337@naver.com>
2024-01-18 17:04:34 +0900

    feat: 기본적인 get API 적용
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/utils/API/todoList.js
+++ src/utils/API/todoList.js
@@ -1,32 +1,5 @@
+import { API_BASE_URL } from "../../constants/API";
+
 export async function getTodoList() {
-  return {
-    해야할일: [
-      {
-        id: 0,
-        title: "투두 아이템 제목 입니다.",
-        content: "투두 아이템 내용입니다.",
-        authorName: "쓴 사람 이름입니다",
-      },
-      {
-        id: 1,
-        title: "투두 아이템 제목 입니다.",
-        content: "투두 아이템 내용입니다.",
-        authorName: "쓴 사람 이름입니다",
-      },
-    ],
-    "하고 있는일": [
-      {
-        id: 2,
-        title: "투두 아이템 제목 입니다.",
-        content: "투두 아이템 내용입니다.",
-        authorName: "쓴 사람 이름입니다",
-      },
-      {
-        id: 3,
-        title: "투두 아이템 제목 입니다.",
-        content: "투두 아이템 내용입니다.",
-        authorName: "쓴 사람 이름입니다",
-      },
-    ],
-  };
+  return (await fetch(`${API_BASE_URL}/todos`)).json();
 }
