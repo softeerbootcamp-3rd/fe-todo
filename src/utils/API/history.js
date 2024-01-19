ec771e438afd1aadc833b7e0c4a1f23b173ac74b
sean2337 <sean2337@naver.com>
2024-01-19 10:35:53 +0900

    feat: 외부 API 관련 파일 제거
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/utils/API/history.js
+++ src/utils/API/history.js
@@ -1,5 +1,3 @@
-import { API_BASE_URL } from "../../constants/API";
-
 export async function getHistory() {
-  return (await fetch(`${API_BASE_URL}/history`, { method: "GET" })).json();
+  return;
 }
--- src/utils/API/todoList.js
+++ src/utils/API/todoList.js
@@ -1,5 +1,3 @@
-import { API_BASE_URL } from "../../constants/API";
-
 export async function getTodoList() {
-  return (await fetch(`${API_BASE_URL}/todos`)).json();
+  return;
 }
