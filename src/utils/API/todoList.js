6c5a2fc94ec3db1a37a92816986d3f43b48874cb
sean2337 <sean2337@naver.com>
2024-01-19 11:53:54 +0900

    feat: todoList API 작성
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/utils/API/todoList.js
+++ src/utils/API/todoList.js
@@ -1,3 +1,5 @@
+import { addHistory, editHistory, moveHistory, removeHistory } from "./history";
+
 export function getTodoList() {
   const todoList = localStorage.getItem("todoList");
   if (todoList !== null) return JSON.parse(todoList);
@@ -11,4 +13,33 @@ export function getTodoList() {
   localStorage.setItem("todoList", JSON.stringify(initialTodoList));
 
   return initialTodoList;
+}
+
+export function addTodoListItem(title, item) {
+  const todoData = JSON.parse(localStorage.getItem("todoList"));
+  todoData[title].unshift(item);
+  localStorage.setItem("todoList", JSON.stringify(todoData));
+  addHistory(title, item);
+}
+
+export function removeTodoListItem(title, index) {
+  const todoData = JSON.parse(localStorage.getItem("todoList"));
+  todoData[title].splice(index, 1);
+  localStorage.setItem("todoList", JSON.stringify(todoData));
+  removeHistory(title, item);
+}
+
+export function editTodoListItem(title, index, item) {
+  const todoData = JSON.parse(localStorage.getItem("todoList"));
+  todoData[title][index] = item;
+  localStorage.setItem("todoList", JSON.stringify(todoData));
+  editHistory(item);
+}
+
+export function moveTodoListItem(titleSrc, indexSrc, titleDst, indexDst) {
+  const todoData = JSON.parse(localStorage.getItem("todoList"));
+  const item = todoData[titleSrc].splice(indexSrc, 1)[0];
+  todoData[titleDst].splice(indexDst, 0, item);
+  localStorage.setItem("todoList", JSON.stringify(todoData));
+  moveHistory(titleSrc, titleDst, item);
 }
