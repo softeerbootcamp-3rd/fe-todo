becb6702f6154ff4b585c73f4726d706a4e72683
sean2337 <sean2337@naver.com>
2024-01-19 11:53:23 +0900

    feat: history API 작성
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/utils/API/history.js
+++ src/utils/API/history.js
@@ -1,8 +1,71 @@
-export async function getHistory() {
+export function getHistory() {
   const historyList = localStorage.getItem("history");
   if (historyList !== null) return JSON.parse(historyList);
 
   // 없으면 새로 생성
   localStorage.setItem("history", JSON.stringify([]));
   return [];
+}
+
+export function addHistory(colTitle, item) {
+  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
+  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
+  const historyItem = {
+    authorName: "멋진삼",
+    timeStamp: new Date().getTime(),
+    actionId: 0,
+    todoTitle: item.title,
+    todoSrc: colTitle,
+    todoDst: null,
+  };
+  historyList.unshift(historyItem);
+  localStorage.setItem("history", JSON.stringify(historyList));
+}
+
+export function removeHistory(colTitle, item) {
+  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
+  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
+  const historyItem = {
+    authorName: "멋진삼",
+    timeStamp: new Date().getTime(),
+    actionId: 1,
+    todoTitle: item.title,
+    todoSrc: colTitle,
+    todoDst: null,
+  };
+
+  historyList.unshift(historyItem);
+  localStorage.setItem("history", JSON.stringify(historyList));
+}
+
+export function editHistory(item) {
+  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
+  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
+  const historyItem = {
+    authorName: "멋진삼",
+    timeStamp: new Date().getTime(),
+    actionId: 2,
+    todoTitle: item.title,
+    todoSrc: null,
+    todoDst: null,
+  };
+
+  historyList.unshift(historyItem);
+  localStorage.setItem("history", JSON.stringify(historyList));
+}
+
+export function moveHistory(colTitleSrc, colTitleDst, item) {
+  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
+  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
+  const historyItem = {
+    authorName: "멋진삼",
+    timeStamp: new Date().getTime(),
+    actionId: 3,
+    todoTitle: item.title,
+    todoSrc: colTitleSrc,
+    todoDst: colTitleDst,
+  };
+
+  historyList.unshift(historyItem);
+  localStorage.setItem("history", JSON.stringify(historyList));
 }
