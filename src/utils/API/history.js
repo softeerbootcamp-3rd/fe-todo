1e9aeeed8efed8738d2ad73028d98eee6e1771c6
sean2337 <sean2337@naver.com>
2024-01-19 10:48:43 +0900

    feat: history API  작성
    Co-authored-by: junhea <97426534+junhea@users.noreply.github.com>

--- src/utils/API/history.js
+++ src/utils/API/history.js
@@ -1,3 +1,8 @@
 export async function getHistory() {
-  return;
+  const historyList = localStorage.getItem("history");
+  if (historyList !== null) return JSON.parse(historyList);
+
+  // 없으면 새로 생성
+  localStorage.setItem("history", JSON.stringify([]));
+  return [];
 }
