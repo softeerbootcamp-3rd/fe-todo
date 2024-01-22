07b85ff90cea125a7dc8b63509d5e1a945519b05
육종호 <dbrwhdgh1203@naver.com>
2024-01-22 12:17:21 +0900

    style: createColumnTemplate 수정

--- src/components/templates.js
+++ src/components/templates.js
@@ -1,13 +1,14 @@
-export function createColumnTemplate(title, id, count) {
-    return `  
-  <header id = "columnHeader">
-    <div class="columnInfo"> 
-        <span>${title}</span>
-        <span class="countBox">${count}</span>
-    </div>
-    <span class="add" ><i id="add" class="fa-solid fa-plus"></i></span>
-    <span id="delete"><i id="deleteCards" class="fa-solid fa-xmark"></i></span>
-  </header>
+export function createColumnTemplate({ title, count }) {
+    return `
+    <header id = "columnHeader">
+      <div class="columnInfo"> 
+          <span>${title}</span>
+          <span class="countBox">${count}</span>
+      </div>
+      <span class="add" ><i id="add" class="fa-solid fa-plus"></i></span>
+      <span id="delete"><i id="deleteCards" class="fa-solid fa-xmark"></i></span>
+    </header>
+
   `;
 }
 export function createEditorTemplate(
