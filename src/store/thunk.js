82899416173e438f4d3db7ffc4ac518dfe3b66ee
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-22 18:20:45 +0900

    chore: 주석 추가

--- src/store/thunk.js
+++ src/store/thunk.js
@@ -1,3 +1,5 @@
+// see https://github.com/reduxjs/redux-thunk/blob/98bffb1c547fb0dac2d8ebe002778bc644d3b2d2/src/index.ts
+
 const createThunkMiddleware = () => {
   const middleware =
     ({ getState, dispatch }) =>
