a49544cdc62d69d0ceb6a0455869e7acb7e30996
Pransinia <trullerster@gmail.com>
2024-01-22 15:17:25 +0900

    ✏️ fix typo at delete card modal

--- src/view/MainView/ColumnView/CardListView/CardView/deleteCardHandler.js
+++ src/view/MainView/ColumnView/CardListView/CardView/deleteCardHandler.js
@@ -37,7 +37,7 @@ export const deleteCardHandler = (target) => {
     deleteCard(target);
   }
   */
-  document.querySelector('#app').insertAdjacentHTML('afterbegin', ModalView({content:"모든 사용자의 활동 기록을 삭제할까요?", btnText:"삭제"}));
+  document.querySelector('#app').insertAdjacentHTML('afterbegin', ModalView({content:"선택한 카드를 삭제할까요?", btnText:"삭제"}));
   document.querySelector('.js-modalConfirm').addEventListener('click', confirmModalHandlerMaker(()=>{
     deleteCard(target);
   }));
