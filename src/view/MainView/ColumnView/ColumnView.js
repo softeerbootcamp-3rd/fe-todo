17bbaee68f9d5336d90c534e27816d35ddcf6122
Pransinia <trullerster@gmail.com>
2024-01-22 17:42:58 +0900

    ♻️ detach model from main view's components

--- src/view/MainView/ColumnView/ColumnView.js
+++ src/view/MainView/ColumnView/ColumnView.js
@@ -1,21 +1,20 @@
-import { columnDataTable } from "../../../model/model.js";
 import { CardListView } from "./CardListView/CardListView.js";
 
-export const ColumnView = (columnId, idx) => {
-  const { title, value } = columnDataTable[columnId];
+//export const ColumnView = (columnId, idx) => {
+export const ColumnView = (idx, title, cardDataList) => {
   return `
   <section class="main__column" id="column${idx}">
     <nav class="main__column__nav">
       <div class="column__nav__info">
         ${ColumnTitleView(title)}
-        ${ColumnCountView(value.length)}
+        ${ColumnCountView(cardDataList.length)}
       </div>
       <div class="column__nav__btn-list">
         <button class="js-addCardBtn column__nav__btn-list__add-card-btn"></button>
         <button class="js-deleteColumnBtn column__nav__btn-list__delete-cloumn-btn"></button>
       </div>
     </nav>
-   ${CardListView(columnId)}
+   ${CardListView(cardDataList, idx)}
   </section>    
     `;
 };
--- src/view/MainView/MainView.js
+++ src/view/MainView/MainView.js
@@ -1,12 +1,16 @@
-import { columnDataTable } from "../../model/model.js";
+import { cardDataTable, columnDataTable } from "../../model/model.js";
 import { ColumnView } from "./ColumnView/ColumnView.js";
 
 export const MainView = () => {
   return `
 <main class="main">
     ${Object.keys(columnDataTable)
-      .map((columnId, idx) => ColumnView(columnId, idx))
+      .map((columnId, idx) => {
+        const cardIdList = columnDataTable[columnId].value;
+        const cardDataList = cardIdList.map((cardId) => [cardId, cardDataTable[cardId]]);
+        return ColumnView(idx, columnDataTable[columnId].title, cardDataList);
+      })
       .join("")}   
-      </main>
+  </main>
 `;
 };
