c3a101106af2c4a102f80a25a1a0593b50285ba1
junhea <97426534+junhea@users.noreply.github.com>
2024-01-24 15:08:26 +0900

    fix: actions MUST be pure functions

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -12,30 +12,33 @@ import { createStore } from "../utils/store";
 export const todoStore = createStore((set, get) => ({
   todoList: getTodoList(), // initial todolist data
   add: (title, item) => {
+    console.log("add request", title, item);
     // add new Item to todoList
     const newItem = addTodoListItem(title, item);
     // returns newly created item (with id)
     set((state) => {
-      const newTodoList = { ...state.todoList };
-      newTodoList[title].unshift(newItem);
-      return { ...state, todoList: newTodoList };
+      const newList = [...state.todoList[title]];
+      newList.unshift(newItem);
+      return { ...state, todoList: { ...state.todoList, [title]: newList } };
     });
   },
   remove: (title, item) => {
+    console.log("remove request", title, item);
     // find and remove item from todoList
     removeTodoListItem(title, item);
     set((state) => {
-      const newTodoList = { ...state.todoList };
-      for (let i = 0; i < newTodoList[title].length; i++) {
-        if (newTodoList[title][i].id === item.id) {
-          newTodoList[title].splice(i, 1);
+      const newList = [...state.todoList[title]];
+      for (let i = 0; i < newList.length; i++) {
+        if (newList[i].id === item.id) {
+          newList.splice(i, 1);
           break;
         }
       }
-      return { ...state, todoList: newTodoList };
+      return { ...state, todoList: { ...state.todoList, [title]: newList } };
     });
   },
   edit: (title, item) => {
+    console.log("edit", title, item);
     editTodoListItem(title, item);
     set((state) => {
       const newTodoList = { ...state.todoList };
