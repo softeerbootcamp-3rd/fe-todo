48c18497f8502d57c6e1d23b88d14ce857e3a35c
junhea <97426534+junhea@users.noreply.github.com>
2024-01-24 15:18:53 +0900

    feat: use TodoStore in todo related components
    attachStore 추가: store에 subscribe해준다(useStore를 사용해서)
    store가 업데이트 될때마다 실행할 콜백을 등록해줌 (updateView)
    
    자식 컴포넌트가 존재할 경우, 생성시 closure를 이용하여 콜백 함수 외부의 변수에 저장해주고, 업데이트 될때마다 mount 해제된 컴포넌트를 찾아 destroy 해준다(컴포넌트 함수에서 리턴하는 cleanup 함수 실행)
    
    attachHandler에서는 listener만 달아준다.
    사용자 상호작용 관련된 코드만 사용.
    attachStore에서 받은 state를 활용하여 action을 호출해줌(상태가 변경된다. 상태의 변경에 따른 결과는 updateView에서만 처리!)
    
    mount는 뷰를 렌더링 해주고 초기 상태를 적용해준다

--- src/components/todoItem/index.js
+++ src/components/todoItem/index.js
@@ -2,12 +2,6 @@ import styles from "./todoItem.module.scss";
 import closedIcon from "../../asset/img/closed.svg";
 import editIcon from "../../asset/img/edit.svg";
 import {
-  addTodoListItem,
-  editTodoListItem,
-  removeTodoListItem,
-} from "../../utils/API/todoList";
-
-import {
   addCheckInput,
   dynamicTextAreaHeight,
   switchMode,
@@ -15,10 +9,47 @@ import {
   requestUpdateItemCount,
   getClosestElement,
 } from "./helper";
+import { todoStore } from "../../stores/todoStore";
+import { getIndexById } from "../../utils/list";
+import { useStore } from "../../utils/store.js";
 
+/*
+  {
+    addMode: true 일 경우 추가 모드
+    listTitle: 투두 리스트 컬럼 제목(키)
+    id: 투두 아이템 인덱스
+  }
+*/
 export default function todoItem(renderTarget, initialData) {
   const views = mount(renderTarget, initialData);
-  return attachHandlers(views, initialData);
+  const store = attachStore(views, initialData);
+  attachHandlers(views, store, initialData);
+  return store.destroy;
+}
+
+function attachStore(
+  { titleNode, contentNode, authorByNode_view, setViewMode },
+  initialData
+) {
+  // dont subscribe if add mode
+  if (initialData.addMode) return { state: todoStore.getState() };
+
+  // state가 업데이트 될때마다 뷰 업데이트
+  const updateView = (item) => {
+    if (item === undefined) return;
+    titleNode.value = item.title;
+    contentNode.value = item.content;
+    authorByNode_view.innerText = `author by ${item.createdOn}`;
+    setViewMode();
+  };
+
+  const store = useStore(todoStore, updateView, (state) => {
+    const list = state.todoList[initialData.listTitle];
+    const i = getIndexById(list, initialData.id);
+    return list[i];
+  });
+
+  return store;
 }
 
 function attachHandlers(
@@ -29,133 +60,64 @@ function attachHandlers(
     contentNode,
     eraseBtnNode_view,
     editBtnNode_view,
-    authorByNode_view,
-    actionBtnContainer_view,
     cancelBtnNode_edit,
     submitBtnNode_edit,
+    setViewMode,
+    setEditMode,
   },
+  { data, state },
   initialData
 ) {
-  // input에 입력이 될때마다 checkInput
-  addCheckInput([titleNode, contentNode], submitBtnNode_edit);
-
-  // 편집 모드 진입시 ui 수정
-  const setEditMode = () =>
-    switchMode({
-      block: [cancelBtnNode_edit, submitBtnNode_edit],
-      none: [authorByNode_view, actionBtnContainer_view],
-      enable: [titleNode, contentNode],
-    });
-
-  // 뷰 모드 진입시 ui 수정
-  const setViewMode = () =>
-    switchMode({
-      block: [actionBtnContainer_view, authorByNode_view],
-      none: [cancelBtnNode_edit, submitBtnNode_edit],
-      disable: [titleNode, contentNode],
-    });
-
-  ////////////////////////////////////////////////////////
-  // 필요 함수 선언 부분
-  // 취소 버튼 클릭 시
+  // 취소 버튼 클릭 시 기존 data로 롤백
   const onCancel_edit = () => {
-    titleNode.value = initialData.item.title;
-    contentNode.value = initialData.item.content;
+    titleNode.value = data.title;
+    contentNode.value = data.content;
     setViewMode();
   };
 
   // 수정/등록 하고 제출 시
-
   const onSubmit_edit = () => {
     const newItem = {
-      ...initialData.item,
+      ...data,
       title: titleNode.value,
       content: contentNode.value,
       createdOn: "web",
     };
 
-    //투두 등록 로직
     if (initialData.addMode) {
-      const newReturnItem = addTodoListItem(initialData.todoColTitle, newItem);
-      //추가하고 추가 컴포넌트 삭제 및
-      initialData.createAndAddItem(newReturnItem);
-    }
-
-    //투두 수정 로직
-    else {
-      editTodoListItem(initialData.todoColTitle, newItem);
-      setViewMode();
+      state.add(initialData.listTitle, newItem);
+      initialData.onCancel();
+    } else {
+      state.edit(initialData.listTitle, newItem);
     }
   };
 
   // 삭제 시
   const onErase_view = () => {
     createDeleteModal(renderTarget, () => {
-      removeTodoListItem(initialData.todoColTitle, initialData.item);
-      const parentNode = renderTarget.parentNode;
-      renderTarget.parentNode.removeChild(renderTarget);
-      requestUpdateItemCount(parentNode);
+      state.remove(initialData.listTitle, data);
     });
   };
-
-  ////////////////////////////////////////////////////////
 
   // textArea의 높이 자동 설정
   dynamicTextAreaHeight(titleNode);
   dynamicTextAreaHeight(contentNode);
-
-  // 함수 이벤트 핸들러 추가하는 부분
-  // 뷰모드 액션 버튼 핸들러 추가
+  // input validation
+  addCheckInput([titleNode, contentNode], submitBtnNode_edit);
+  // 뷰모드 액션 버튼
   editBtnNode_view.addEventListener("click", setEditMode);
   eraseBtnNode_view.addEventListener("click", onErase_view);
-
-  // 편집모드 버튼 핸들러 추가
+  // 편집모드 버튼
   cancelBtnNode_edit.addEventListener(
     "click",
     initialData.onCancel ?? onCancel_edit
   );
   submitBtnNode_edit.addEventListener("click", onSubmit_edit);
 
-  // 드래그 이벤트 처리 (생성 카드 제외)
+  // 드래그 이벤트 처리 (생성 모드 제외)
   if (!initialData.addMode) {
-    renderTarget.addEventListener("dragstart", (e) => {
-      e.dataTransfer.setData(
-        "text/plain",
-        JSON.stringify({
-          ...initialData.item,
-          todoColTitle: initialData.todoColTitle,
-        })
-      );
-      e.dataTransfer.dropEffect = "move";
-      todoItem.classList.add(styles["todoItem--dragging"]);
-    });
-
-    renderTarget.addEventListener("dragend", (e) => {
-      e.preventDefault();
-      todoItem.classList.remove(styles["todoItem--dragging"]);
-      requestUpdateItemCount(renderTarget, true);
-    });
-
-    renderTarget.addEventListener("dragover", (e) => {
-      e.stopPropagation();
-      e.preventDefault();
-      const elem = getClosestElement(
-        [...renderTarget.parentNode.querySelectorAll(`[data-node="todoItem"]`)],
-        e.clientY
-      );
-      const dragging = document.querySelector(
-        `div.${styles["todoItem--dragging"]}`
-      ).parentNode;
-      renderTarget.parentNode.insertBefore(dragging, elem?.parentNode);
-      requestUpdateItemCount(renderTarget, true);
-    });
+    enableDrag({ renderTarget, todoItem }, { data }, initialData);
   }
-
-  // 투두 아이템의 초기 모드를 뷰 모드로 설정
-  if (initialData.addMode) setEditMode();
-  else setViewMode();
-
-  return () => {};
 }
 
 // todoItem 컴포넌트 템플릿
@@ -171,17 +133,17 @@ function mount(renderTarget, initialData) {
         data-node="title"
         class="${styles.todoItem__itemTitle}"
         placeholder="제목을 입력하세요"
-      >${initialData.item?.title ?? ""}</textarea>
+      ></textarea>
       <textarea
         type="text"
         rows="1"
         data-node="content"
         class="${styles.todoItem__itemContent}"
         placeholder="내용을 입력하세요"
-      >${initialData.item?.content ?? ""}</textarea>
+      ></textarea>
       <div class="${styles.todoItem__bottomContainer}">
         <p data-node="author" class="${styles.todoItem__itemAuthor}">
-          author by ${initialData.item?.createdOn}
+          
         </p>
         <button
           data-node="cancelBtn"
@@ -231,6 +193,26 @@ function mount(renderTarget, initialData) {
     '[data-node="submitBtn"]'
   );
 
+  // 편집 모드 진입시 ui 수정
+  const setEditMode = () =>
+    switchMode({
+      block: [cancelBtnNode_edit, submitBtnNode_edit],
+      none: [authorByNode_view, actionBtnContainer_view],
+      enable: [titleNode, contentNode],
+    });
+
+  // 뷰 모드 진입시 ui 수정
+  const setViewMode = () =>
+    switchMode({
+      block: [actionBtnContainer_view, authorByNode_view],
+      none: [cancelBtnNode_edit, submitBtnNode_edit],
+      disable: [titleNode, contentNode],
+    });
+
+  // 초기 모드 설정
+  if (initialData?.addMode) setEditMode();
+  else setViewMode();
+
   return {
     renderTarget,
     todoItem,
@@ -242,5 +224,42 @@ function mount(renderTarget, initialData) {
     actionBtnContainer_view,
     cancelBtnNode_edit,
     submitBtnNode_edit,
+    setViewMode,
+    setEditMode,
   };
+}
+
+function enableDrag({ renderTarget, todoItem }, { data }, initialData) {
+  renderTarget.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData(
+      "text/plain",
+      JSON.stringify({
+        ...data,
+        listTitle: initialData.listTitle,
+      })
+    );
+    e.dataTransfer.dropEffect = "move";
+    todoItem.classList.add(styles["todoItem--dragging"]);
+  });
+
+  renderTarget.addEventListener("dragend", (e) => {
+    e.preventDefault();
+    todoItem.classList.remove(styles["todoItem--dragging"]);
+    requestUpdateItemCount(renderTarget, true);
+  });
+
+  renderTarget.addEventListener("dragover", (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    const elem = getClosestElement(
+      [...renderTarget.parentNode.querySelectorAll(`[data-node="todoItem"]`)],
+      e.clientY
+    );
+    const dragging = document.querySelector(
+      `div.${styles["todoItem--dragging"]}`
+    ).parentNode;
+    renderTarget.parentNode.insertBefore(dragging, elem?.parentNode);
+    //TODO: implement this using store
+    //requestUpdateItemCount(renderTarget, true);
+  });
 }
