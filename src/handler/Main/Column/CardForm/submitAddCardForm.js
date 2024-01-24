fae64e2648d2ffeb721bfc041722098bbab7eadd
Sang-minKIM <kimpran@naver.com>
2024-01-24 19:34:39 +0900

    refactor: move cardId to Store

--- src/handler/Main/Column/CardForm/submitAddCardForm.js
+++ src/handler/Main/Column/CardForm/submitAddCardForm.js
@@ -3,19 +3,18 @@ import { getDeviceInfo } from "@/util/getDeviceInfo.js";
 import { renderCardList } from "@/view/Main/Column/renderCardList.js";
 import { renderListCount } from "@/view/Main/Column/renderListCount.js";
 
-let cardId = 3;
-
 const createCardData = (target) => {
   const formData = new FormData(target);
   formData.set("author", getDeviceInfo());
   formData.set("createdAt", new Date());
-  formData.set("cardId", cardId);
+  formData.set("cardId", store.cardId);
   return Object.fromEntries(formData);
 };
 
 export const submitAddCardForm = (target) => {
   const currentColumn = target.closest("section");
   const cardData = createCardData(target);
+  // store.addCardToServer({ columnId: currentColumn.id, cardData });
   store.addCard({ columnId: currentColumn.id, cardData });
   store.setAddCardHistory(currentColumn.id);
   renderCardList(currentColumn);
