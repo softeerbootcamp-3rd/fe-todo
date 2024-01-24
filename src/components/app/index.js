685dfb27dc117cedef942087eeb643c903a39c72
sean2337 <sean2337@naver.com>
2024-01-24 14:25:42 +0900

    refactor: id명에 대한 코드 리뷰 반영

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -6,48 +6,38 @@ import modal from "../modal";
 import { applyDragAndDrop } from "../../utils/dragAndDrop";
 import { store } from "../../store/todoStore";
 
-const newTodoItem = {
-  colName: "해야할 일",
-  item: {
-    title: "추가",
-    content: "추가",
-    createdOn: "Mobile",
-  },
-};
-
 export default function App(parent, props) {
   parent.innerHTML = `
         <div class="${styles.app}">
-          <div todo-data="headerSection"></div>
-          <div todo-data="todoSection"></div>
-          <div todo-data="historySection" class="${styles.app__historySection}"></div>
-          <div todo-data="modalSection" class="${styles.app__modalSection}"></div>
+          <div todo-section="headerSection"></div>
+          <div todo-section="todoSection"></div>
+          <div todo-section="historySection" class="${styles.app__historySection}"></div>
+          <div todo-section="modalSection" class="${styles.app__modalSection}"></div>
         </div>
     `;
 
   //헤더 컴포넌트 마운트
-  const headerSection = parent.querySelector('[todo-data="headerSection"]');
-  headerSection.addEventListener("click", () => {
-    store.dispatch({ type: "plusTodoItem", payload: newTodoItem });
-  });
+  const headerSection = parent.querySelector('[todo-section="headerSection"]');
   header(headerSection, {});
 
   //todo컴포넌트 마운트
   //=> 처음에 초기값, API 모든 데이터를 넣어줘야함.
   //=> 생성, 수정, 삭제는 API요청은 있으나 다시 받아오지는 않음.
 
-  const todoSection = parent.querySelector('[todo-data="todoSection"]');
+  const todoSection = parent.querySelector('[todo-section="todoSection"]');
   todoListTable(todoSection, { todoList: store.getTodoList() });
 
   //클릭 시 History 컴포넌트 마운트
-  const historySection = parent.querySelector('[todo-data="historySection"]');
+  const historySection = parent.querySelector(
+    '[todo-section="historySection"]'
+  );
   document.addEventListener("toggleHistoryList", () => {
     historySection.classList.toggle(styles["app__historySection--show"]);
     todoHistory(historySection, {});
   });
 
   // 모달 섹션
-  const modalSection = parent.querySelector('[todo-data="modalSection"]');
+  const modalSection = parent.querySelector('[todo-section="modalSection"]');
   //배경 클릭 시 모달 삭제
   modalSection.addEventListener("click", () => {
     modalSection.style.display = "none";
