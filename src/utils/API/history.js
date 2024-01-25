f0be6083aca007008e32f9d71ec033f7cf261f82
junhea <97426534+junhea@users.noreply.github.com>
2024-01-25 17:15:08 +0900

    feat: history api and store impl

--- src/utils/API/history.js
+++ src/utils/API/history.js
@@ -1,75 +1,9 @@
-export function getHistory() {
-  const historyList = localStorage.getItem("history");
-  if (historyList !== null) return JSON.parse(historyList);
+import { API_BASE_URL, httpDelete, httpGet } from "./http";
 
-  // 없으면 새로 생성
-  localStorage.setItem("history", JSON.stringify([]));
-  return [];
+export async function getHistory() {
+  return (await httpGet(`${API_BASE_URL}/history`)).json();
 }
 
-export function clearHistory() {
-  localStorage.setItem("history", JSON.stringify([]));
-}
-
-export function addHistory(colTitle, item) {
-  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
-  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
-  const historyItem = {
-    authorName: "멋진삼",
-    timeStamp: new Date().getTime(),
-    actionId: 0,
-    todoTitle: item.title,
-    todoSrc: colTitle,
-    todoDst: null,
-  };
-  historyList.unshift(historyItem);
-  localStorage.setItem("history", JSON.stringify(historyList));
-}
-
-export function removeHistory(colTitle, item) {
-  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
-  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
-  const historyItem = {
-    authorName: "멋진삼",
-    timeStamp: new Date().getTime(),
-    actionId: 1,
-    todoTitle: item.title,
-    todoSrc: colTitle,
-    todoDst: null,
-  };
-
-  historyList.unshift(historyItem);
-  localStorage.setItem("history", JSON.stringify(historyList));
-}
-
-export function editHistory(item) {
-  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
-  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
-  const historyItem = {
-    authorName: "멋진삼",
-    timeStamp: new Date().getTime(),
-    actionId: 2,
-    todoTitle: item.title,
-    todoSrc: null,
-    todoDst: null,
-  };
-
-  historyList.unshift(historyItem);
-  localStorage.setItem("history", JSON.stringify(historyList));
-}
-
-export function moveHistory(colTitleSrc, colTitleDst, item) {
-  const historyList = JSON.parse(localStorage.getItem("history")) ?? [];
-  // actionId = 0: 등록 / 1: 삭제 / 2: 변경 / 3: 이동
-  const historyItem = {
-    authorName: "멋진삼",
-    timeStamp: new Date().getTime(),
-    actionId: 3,
-    todoTitle: item.title,
-    todoSrc: colTitleSrc,
-    todoDst: colTitleDst,
-  };
-
-  historyList.unshift(historyItem);
-  localStorage.setItem("history", JSON.stringify(historyList));
+export async function clearHistory() {
+  return httpDelete(`${API_BASE_URL}/history`);
 }
