8bf94c0cd991e2fb5fccf4436fa0f6ff5052bf1d
Jeongjin Oh <rojay.developer@gmail.com>
2024-01-25 18:54:16 +0900

    feat: 사용자 기록 API 연동

--- src/components/action-history-list/index.js
+++ src/components/action-history-list/index.js
@@ -1,6 +1,7 @@
 import { setEvent } from "../../utils/set-event.js";
 import * as ActionHistoryItem from "../action-history-item/index.js";
 import * as Alert from "../alert/index.js";
+import * as history from "../../features/action-history/index.js";
 
 const app = document.getElementById("app");
 
@@ -8,7 +9,17 @@ export function template({ history } = { history: [] }) {
   return `
     <dialog class="action-history-dialog rounded-16 shadow-floating">
       <div class="action-history__container">
-        <div class="action-history__header">
+      </div>
+    </dialog>
+    `;
+}
+
+export function render({ history }) {
+  const actionHistoryContainer = document.querySelector(
+    ".action-history__container"
+  );
+
+  actionHistoryContainer.innerHTML = `<div class="action-history__header">
           <span class="text-strong display-bold16">사용자 활동 기록</span>
           <button
             class="action-history__close-button text-default display-bold14"
@@ -21,31 +32,30 @@ export function template({ history } = { history: [] }) {
               alt="닫기"
             />
             닫기
-          </button>
+            </button>
         </div>
         <ul class="action-history__items">
-          ${history.map((item) => ActionHistoryItem.template()).join("")}
+          ${
+            history.length === 0
+              ? `<p class="text-weak display-medium14" style="text-align:center; padding:16px">사용자 활동 기록이 없습니다.</p>`
+              : history
+                  .map((item) => ActionHistoryItem.template({ history: item }))
+                  .join("")
+          }
         </ul>
-        <div class="action-history__footer">
-          <button
-            class="history-reset-button button text-danger display-bold14"
-            style="width: 104px"
-            type="button"
-          >
-            기록 전체 삭제
-          </button>
-        </div>
-      </div>
-    </dialog>
-    `;
-}
-
-export function render({ history }) {
-  const actionHistoryItems = document.querySelector(".action-history__items");
-
-  actionHistoryItems.innerHTML = `${history
-    .map((item) => ActionHistoryItem.template({ history: item }))
-    .join("")}`;
+        ${
+          history.length === 0
+            ? ""
+            : `<div class='action-history__footer'>
+              <button
+                class='history-reset-button button text-danger display-bold14'
+                style='width: 104px'
+                type='button'
+              >
+                기록 전체 삭제
+              </button>
+            </div>`
+        }`;
 }
 
 export function show() {
@@ -69,8 +79,12 @@ setEvent(app, "click", (event) => {
 
   Alert.show({
     message: "모든 사용자 활동 기록을 삭제할까요?",
-    onConfirm: () => {
-      console.log("사용자 활동 기록 삭제");
+    onConfirm: async () => {
+      await history.resetHistory({
+        onChange: (state) => {
+          render({ history: state.history });
+        },
+      });
     },
   });
 });
