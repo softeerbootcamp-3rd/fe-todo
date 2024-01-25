c919821184e6981be9db36e86f41dd0cde659bd9
육종호 <dbrwhdgh1203@naver.com>
2024-01-25 19:23:08 +0900

    refactor: initializeApp 함수 제거

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 import { createColumns } from "./src/components/columns.js";
 import { createLogBox } from "./src/components/log.js";
-import { TodoStore } from "./src/Store/store.js";
+import todoStore from "./src/Store/store.js";
 
-(() => {
+(async () => {
     const app = document.getElementById("app");
-
-    async function initializeApp() {
-        const store = new TodoStore();
-        await store.fetchInitialData();
-        createColumns(app, store);
-        createLogBox(app);
-    }
-
-    initializeApp();
+    const columnData = todoStore.getState();
+    createLogBox(app);
+    createColumns(app, columnData);
 })();
