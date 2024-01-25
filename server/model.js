73c22d9c4515a128c830ced04b8e0a54aea5ac08
junhea <97426534+junhea@users.noreply.github.com>
2024-01-25 15:30:09 +0900

    feat: [server] mock data

---
+++ server/model.js
@@ -0,0 +1,66 @@
+const todo = {
+  "해야할 일": [
+    {
+      title: "3",
+      content: "3",
+      createdOn: "web",
+      id: 3,
+    },
+    {
+      title: "2",
+      content: "2",
+      createdOn: "web",
+      id: 2,
+    },
+    {
+      title: "1",
+      content: "1",
+      createdOn: "web",
+      id: 1,
+    },
+  ],
+  "하고 있는 일": [
+    {
+      title: "6",
+      content: "6",
+      createdOn: "web",
+      id: 6,
+    },
+    {
+      title: "5",
+      content: "5",
+      createdOn: "web",
+      id: 5,
+    },
+    {
+      title: "4",
+      content: "4",
+      createdOn: "web",
+      id: 4,
+    },
+  ],
+  "완료한 일": [
+    {
+      title: "9",
+      content: "9",
+      createdOn: "web",
+      id: 9,
+    },
+    {
+      title: "8",
+      content: "8",
+      createdOn: "web",
+      id: 8,
+    },
+    {
+      title: "7",
+      content: "7",
+      createdOn: "web",
+      id: 7,
+    },
+  ],
+};
+
+const history = [];
+
+module.exports = { todo, history };
