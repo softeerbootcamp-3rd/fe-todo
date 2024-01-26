8a1de58105c9375db2b7899acd9d4f4f540061dd
Sang-minKIM <kimpran@naver.com>
2024-01-26 10:27:14 +0900

    chore: remove sudo code

--- src/model/Store.js
+++ src/model/Store.js
@@ -9,49 +9,6 @@ import {
 import { deepCopy } from "../util/deepCopy";
 import { getCardsObject, getColumnsObject } from "../util/syncData";
 
-// class Column {
-//   constructor() {
-//     this._columns = {};
-//   }
-//   addCardToColumn(columnId, cardId) {
-//     const column = this._columns[columnId];
-//     column.value.unshift(cardId);
-//   }
-// }
-
-// class Card {
-//   constructor() {
-//     this._cards = {};
-//   }
-
-//   addCardData(cardData, cardId) {
-//     this._cards[cardId] = cardData;
-//   }
-// }
-
-// class History {
-//   constructor() {
-//     this._history = {};
-//   }
-//   addHistory(newHistory) {
-//     this._history.unshift(newHistory);
-//   }
-// }
-
-// class Storessss {
-//   constructor() {
-//     this._cardId = 3;
-//     this.cardManager = new Card();
-//     this.columnManager = new Column();
-//     this.historyManager = new History();
-//   }
-//   addCard({ columnId, cardData }) {
-//     this.cardManager.addCardData(cardData, this._cardId);
-//     this.columnManager.addCardToColumn(columnId, this._cardId);
-//     this._cardId++;
-//   }
-// }
-
 class Store {
   constructor() {
     this._cardId = 0;
