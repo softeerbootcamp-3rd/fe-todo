d76aba0a4213cd50126602703d695e3305b9b1e1
육종호 <dbrwhdgh1203@naver.com>
2024-01-26 12:20:30 +0900

    feat: 카드 등록 및 삭제 시 상태 처리 기능 구현

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,4 +1,5 @@
 import { getData } from "../services/http.js";
+import { createUniqueId } from "../utils/createUniqueId.js";
 
 class Store {
     constructor(initialState = []) {
@@ -10,9 +11,11 @@ class Store {
         this.events.push(eventCallback);
     }
 
-    notify() {
+    notify(payload) {
+        if (!this.events) return;
+
         this.events.forEach((eventCallback) => {
-            eventCallback();
+            eventCallback(payload);
         });
     }
 
@@ -20,11 +23,37 @@ class Store {
         return this.state;
     }
 
-    setState(state) {
-        this.state = state;
+    setState() {}
+
+    registeredCard({ columnIndex, title, content }) {
+        const newCard = {
+            id: createUniqueId(),
+            title,
+            content,
+        };
+        const newCardList = [newCard, ...this.state[columnIndex]["cardList"]];
+        this.state[columnIndex]["count"] = newCardList.length;
+        this.state[columnIndex]["cardList"] = newCardList;
+
+        const columnId = this.state[columnIndex]["id"];
+        const columnCnt = this.state[columnIndex]["count"];
+        const payload = {
+            columnId,
+            newCardList,
+            columnCnt,
+        };
+        console.log(payload);
+        this.notify(payload);
     }
 
-    addNewCard() {}
+    deleteCard({ columnIndex, id }) {
+        this.state[columnIndex]["cardList"] = this.state[columnIndex][
+            "cardList"
+        ].filter((element) => {
+            element.id !== id;
+        });
+        this.notify(this.state[columnIndex]["cardList"]);
+    }
 }
 
 const getColumnData = await getData();
